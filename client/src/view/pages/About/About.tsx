441b3635226e70b04a5391ceb88f613f8b28c1b5
Sithum Imesh <sithum.imesh052002@icloud.com>
2024-08-14 22:50:07 +0530

    About Complete

--- client/src/view/pages/About/About.tsx
+++ client/src/view/pages/About/About.tsx
@@ -1,5 +1,7 @@
 import iphone13 from '../../../images/iPhone_13.png'
 import laptopBG from '../../../images/laptopBg.jpg'
+import CIcon from "@coreui/icons-react";
+import {cilPaperPlane} from "@coreui/icons";
 
 export function About() {
     return (
@@ -25,7 +27,60 @@ export function About() {
                 </div>
             </div>
 
-            <div className="flex justify-between mt-[8%] ">
+
+            <div className="mt-[8%]">
+                <div
+                    className="grid lg:grid-rows-1 md:grid-rows-2 grid-cols-4 flex-wrap w-full lg:w-auto bg-white mt-[2%]">
+                    <div
+                        className="flex lg:col-span-1 md:col-span-2 col-span-4 flex-col items-center p-12 gap-5">
+                            <span className="material-symbols-outlined text-primary text-[44px] font-light">
+                                home
+                            </span>
+
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-5xl font-extra-light">2345</h1>
+                            <p className="text-[14px] text-text mt-[3%]">goods available for purchase</p>
+                        </div>
+                    </div>
+
+                    <div
+                        className=" flex lg:col-span-1 md:col-span-2 col-span-4 flex-col items-center p-12 gap-5">
+                        <CIcon className="text-primary size-10" icon={cilPaperPlane}/>
+
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-5xl font-extra-light">3758</h1>
+                            <p className="text-[14px] text-text mt-[5%]">returning customers</p>
+                        </div>
+                    </div>
+
+                    <div
+                        className=" lg:col-span-1 md:col-span-2 col-span-4 flex flex-col items-center p-12 gap-5">
+               <span className="material-symbols-outlined text-primary text-[44px] font-light">
+                        help
+               </span>
+
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-5xl font-extra-light">837</h1>
+                            <p className="text-[14px] text-text mt-[3%]">of users visited</p>
+                        </div>
+                    </div>
+
+                    <div
+                        className="border-gray-200] flex lg:col-span-1 md:col-span-2 col-span-4 flex-col items-center p-12 gap-5">
+               <span className="material-symbols-outlined text-primary text-[44px] font-light">
+                        rocket_launch
+               </span>
+
+                        <div className="flex flex-col items-center">
+                            <h1 className="text-5xl font-extra-light">1285</h1>
+                            <p className="text-[14px] text-text mt-[3%]">new arrivals</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="flex justify-between my-[8%] ">
 
                 <div className="flex justify-end w-1/2">
                     <img src={laptopBG} className="w-full shadow-md rounded-xl" alt=""/>
