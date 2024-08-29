e73a5fca9947cb16024f93914320d55adde0a6f7
Sithum Imesh <sithum.imesh052002@icloud.com>
2024-08-29 21:09:07 +0530

    sale item bugfix

--- client/src/view/common/SaleItemCard/SaleItemCard.tsx
+++ client/src/view/common/SaleItemCard/SaleItemCard.tsx
@@ -19,7 +19,6 @@ export function SaleItemCard() {
         const filtered = productList.filter(item => item.sale);
         // @ts-ignore
         setSaleProducts(filtered);
-        console.log(filtered)
     }, []);
 
     useEffect(() => {
@@ -59,7 +58,7 @@ function ProductCard({ product, timeLeft }: ProductCardProps) {
         <div className="flex flex-col lg:flex-row items-center border border-gray-200 hover:border-primary transition-all duration-300 hover:scale-105 rounded-xl p-4">
             <ProductImage image={product.image} model={product.model} />
             <div className="flex flex-col flex-grow ml-3">
-                <RatingStars rating={4} />
+                <RatingStars product={product} />
                 <ProductInfo product={product} />
                 <CountdownTimer timeLeft={timeLeft} />
             </div>
@@ -76,14 +75,26 @@ function ProductImage({ image, model }: { image: string; model: string }) {
     );
 }
 
-function RatingStars({ rating }: { rating: number }) {
+function RatingStars({ product }: { product: Product }) {
+    const ratingCounts = product.reviews.reduce((acc, review) => {
+        acc[review.rate] = (acc[review.rate] || 0) + 1;
+        return acc;
+    }, {} as Record<number, number>);
+
+    const mostCommonHighestRate = Object.entries(ratingCounts).reduce((a, b) =>
+        a[1] > b[1] ? a : b[1] === a[1] ? (Number(b[0]) > Number(a[0]) ? b : a) : b
+    );
+
+    const [displayedRate, displayedCount] = mostCommonHighestRate;
+
     return (
-        <div className="flex mb-2 justify-center sm:justify-start">
+        <div className="flex mb-2 justify-center sm:justify-start items-center">
             {[...Array(5)].map((_, i) => (
-                <svg key={i} className={`w-4 h-4 fill-current ${i < rating ? 'text-yellow-400' : 'text-gray-300'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <svg key={i} className={`w-4 h-4 fill-current ${i < Number(displayedRate) ? 'text-yellow-400' : 'text-gray-300'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.817 1.508 8.213L12 18.897l-7.444 4.439 1.508-8.213L.587 9.306l8.332-1.151z"/>
                 </svg>
             ))}
+            {/*<span className="ml-2 text-sm text-gray-600">({displayedCount})</span>*/}
         </div>
     );
 }
